c84da2cf87e6504a69646647fd247b2ba4a6112f
valikira914-ai <vali.kira914@gmail.com>
2026-09-05 22:36:55 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#2222c5",
+      secondary: "050301",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
